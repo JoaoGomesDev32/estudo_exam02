2dda4c80960785d874113057f523bd1e968b893b
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-06-29 15:13:44 +0100

    falta ajustar o first_word

---
+++ lv0/first_word.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    if (argc != 2)
+        ft_putchar('\n');
+    else
+    {
+        i = 0;
+        while (argv[1][i] == '\t' || argv[1][i] == ' ')
+            i++;
+        while (argv[1][i] && (argv[1][i] != '\t' || argv[1][i] != ' '))
+        {
+            ft_putchar(argv[1][i]);
+            i++;
+        }
+        ft_putchar('\n');
+        return (0);
+    }
+}
