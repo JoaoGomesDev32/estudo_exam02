42096a4ead797a6591ce227f83f79cd3096203d7
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-06 21:47:52 +0100

    search_and_replace done

--- lv0/search_and_replace.c
+++ lv0/search_and_replace.c
@@ -5,18 +5,21 @@ int main(int argc, char **argv)
     int i;
 
     i = 0;
-    if (argc == 4)
+	if (argc != 4)
+		write(1, "\n", 1);
+	else if (argv[2][0] == '\0' || argv[3][0] == '\0' || argv[2][1] != '\0' || argv[3][1] != '\0')
+		write(1, "\n", 1);
+    else
     {
-        while (argv[1][i] && (argv[1][i] >= ' ' || argv[1][i] <= '~'))
+        while (argv[1][i])
         {
             if (argv[1][i] == argv[2][0])
-                argv[1][i] = argv[3][0];
-        else if ((argv[2][0] != '\0' && argv[3][0] != '\0') && (argv[2][1] == '\0' && argv[3][1] == '\0'))
-            write(1, "\n", 1);
-        write(1, &argv[1][i], 1);
+				write(1, &argv[3][0], 1);
+			else
+                write(1, &argv[1][i], 1);
         i++;
         }
+		write(1, "\n", 1);
     }
-    write(1, "\n", 1);
     return (0);
 }
