6b947e3e17f2a7783f1b587102fdd98e5f9b92b8
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-07 22:12:11 +0100

    last_word done

---
+++ lv1/last_word.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc != 2)
+		write(1, "\n", 1);
+	else
+	{
+		while (argv[1][i])
+			i++;
+		i--;
+		while (argv[1][i] == ' ' || argv[1][i] == '\t')
+			i--;
+		if (i == 0)
+			write(1, "\n", 1);
+		while (i > 0 && argv[1][i - 1] != ' ' && argv[1][i - 1] != '\t')
+			i--;
+		while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+		{
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+	}
+	return (0);
+}
