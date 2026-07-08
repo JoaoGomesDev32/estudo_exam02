b13c7c24a4c3b4002a4fb6df8132c35afe90bf63
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-08 13:02:39 +0100

    ft_strdup done

---
+++ lv1/ft_strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	while (src[i])
+		i++;
+	dest = malloc(i + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
