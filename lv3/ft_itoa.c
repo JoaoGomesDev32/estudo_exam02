bfbe2c0bfd6ed0e1a8b0e9e4d5d130131c55bf57
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-16 15:28:04 +0100

    ft_atoa done

---
+++ lv3/ft_itoa.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+int	ft_numlen(int n)
+{
+	int len;
+
+	len = (n <= 0) ? 1 : 0;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int		len;
+	char	*str;
+	long	nb;
+
+	nb = n;
+	len = ft_numlen(n);
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	if (nb == 0)
+		str[0] = '0';
+	while (nb > 0)
+	{
+		str[--len] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	return (str);
+}
