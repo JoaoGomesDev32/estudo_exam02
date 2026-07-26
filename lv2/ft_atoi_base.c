9d247fb1f5f077173d448f8e2f53f4fa80f280dc
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-26 10:23:41 +0100

    ft_atoi_base

--- lv2/ft_atoi_base.c
+++ lv2/ft_atoi_base.c
@@ -6,7 +6,30 @@ int	char_to_val(char c)
 		return (c - 'a' + 10);
 	else if (c >= 'A' && c <= 'F')
 		return (c - 'A' + 10);
+	return (-1);
 }
 
 int	ft_atoi_base(const char *str, int str_base)
-{}
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	res = 0;
+	while (char_to_val(str[i]) >= 0 && char_to_val(str[i]) < str_base)
+	{
+		res = (res * str_base + char_to_val(str[i]));
+		i++;
+	}
+	return (res * sign);
+}
