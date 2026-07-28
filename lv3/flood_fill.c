8af1e67c42ceab043e9443873ffd7398a12fda14
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-28 12:51:47 +0100

    flood_fill done

---
+++ lv3/flood_fill.c
@@ -0,0 +1,23 @@
+typedef struct  s_point
+{
+int           x;
+int           y;
+}               t_point;
+
+void    fill(char **tab, t_point size, t_point cur, char target)
+{
+    if (cur.x < 0 || cur.y < 0 || cur.x >= size.x || cur.y >= size.y)
+        return ;
+    if (tab[cur.y][cur.x] != target)
+        return ;
+    tab[cur.y][cur.x] = 'F';
+    fill(tab, size, (t_point){cur.x + 1, cur.y}, target);
+    fill(tab, size, (t_point){cur.x - 1, cur.y}, target);
+    fill(tab, size, (t_point){cur.x, cur.y + 1}, target);
+    fill(tab, size, (t_point){cur.x, cur.y - 1}, target);
+}
+
+void  flood_fill(char **tab, t_point size, t_point begin)
+{
+    fill(tab, size, begin, tab[begin.y][begin.x]);
+}
