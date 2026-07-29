16918d1ae7a6262bced04d96802030fcff8d851e
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-29 15:01:14 +0100

    rev_wstr done

---
+++ lv3/rev_wstr.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+void	print_word(char *str, int start, int end)
+{
+	while (start < end)
+	{
+		write(1, &str[start], 1);
+		start++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	end;
+
+	if (ac != 2)
+		write(1, "\n", 1);
+	else
+	{
+		i = 0;
+		while (av[1][i])
+			i++;
+		while (i >= 0)
+		{
+			end = i;
+			while (i > 0 && av[1][i - 1] != ' ')
+				i--;
+			print_word(av[1], i, end);
+			if (i > 0)
+			{
+				write(1, " ", 1);
+				i--;
+			}
+		}
+		write(1, "\n", 1);
+	}
+	return (0);
+}
