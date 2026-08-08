3e36c4ade974dc8dd153c1f9b86f6b28b9708f20
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-08 11:05:18 +0100

    wdmatch done

--- lv1/wdmatch.c
+++ lv1/wdmatch.c
@@ -12,15 +12,15 @@ int	main(int ac, char **av)
 		while (av[2][j])
 		{
 			if (av[2][j] == av[1][i])
-				j++;
-			i++;
+				i++;
+			j++;
 		}
 		if (!av[1][i])
 		{
 			i = 0;
 			while (av[1][i])
 			{
-				write(1 &av[1][i], 1);
+				write(1, &av[1][i], 1);
 				i++;
 			}
 		}
