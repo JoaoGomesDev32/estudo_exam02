fc6d1339d3d0111c92d7d065375b63ea9dc6ae23
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-08 10:58:26 +0100

    improve wdmatch

--- lv1/wdmatch.c
+++ lv1/wdmatch.c
@@ -1,36 +1,29 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int	main(int ac, char **av)
 {
-	int	i;
-	int	j;
-	int	k;
+	int	i = 0;
+	int	j = 0;
 
-	if (argc != 3)
-	{
+	if (ac != 3)
 		write(1, "\n", 1);
-		return (0);
-	}
 	else
 	{
-		i = 0;
-		j = 0;
-		k = 0;
-		while (argv[2][j])
+		while (av[2][j])
 		{
-			if (argv[2][j] == argv[1][i])
-				i++;
-			j++;
+			if (av[2][j] == av[1][i])
+				j++;
+			i++;
 		}
-		if (!argv[1][i])
+		if (!av[1][i])
 		{
-			while (argv[1][k])
+			i = 0;
+			while (av[1][i])
 			{
-				write(1, &argv[1][k], 1);
-				k++;
+				write(1 &av[1][i], 1);
+				i++;
 			}
 		}
+		write(1, "\n", 1);
 	}
-	write(1, "\n", 1);
-	return (0);
 }
