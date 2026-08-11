1b1024d44b1136d8e1235f83199815f975dc5832
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-11 15:15:19 +0100

    change variable name

--- lv2/ft_rrange.c
+++ lv2/ft_rrange.c
@@ -3,24 +3,24 @@
 int	*ft_rrange(int start, int end)
 {
 	int	i;
-	int	*rrange;
+	int	*arr;
 
 	if (start > end)
-		rrange = malloc(sizeof(int) * (start - end + 1));
+		arr = malloc(sizeof(int) * (start - end + 1));
 	else
-		rrange = malloc(sizeof(int) * (end - start + 1));
-    if (!rrange)
+		arr = malloc(sizeof(int) * (end - start + 1));
+	if (!arr)
 		return (NULL);
 	i = 0;
 	while (start != end)
 	{
-		rrange[i] = end;
+		arr[i] = end;
 		if (start > end)
 			end++;
 		else
 			end--;
 		i++;
 	}
-	rrange[i] = end;
-	return (rrange);
+	arr[i] = end;
+	return (arr);
 }
