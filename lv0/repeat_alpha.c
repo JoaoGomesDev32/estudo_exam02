09b74f45dc1ec3b3fa23bee7d5fc33ce455dd298
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-11 14:53:22 +0100

    repeat_alpha improve

--- lv0/repeat_alpha.c
+++ lv0/repeat_alpha.c
@@ -2,8 +2,9 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	j;
+	int i = 0;
+    int j;
+    int rep;
 
 	i = 0;
 	if (argc != 2)
@@ -12,27 +13,18 @@ int	main(int argc, char **argv)
 	{
 		while (argv[1][i])
 		{
-			j = 1;
 			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-			{
-				j = argv[1][i] - 'a' + 1;
-				while (j > 0)
-				{
-					write(1, &argv[1][i], 1);
-					j--;
-				}
-			}
+				rep = argv[1][i] - 'a' + 1;
 			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-			{
-				j = argv[1][i] - 'A' + 1;
-				while (j > 0)
-				{
-					write(1, &argv[1][i], 1);
-					j--;
-				}
-			}
+				rep = argv[1][i] - 'A' + 1;
 			else
+				rep = 1;
+			j = 0;
+			while (j < rep)
+			{
 				write(1, &argv[1][i], 1);
+				j++;
+			}
 			i++;
 		}
 		write(1, "\n", 1);
