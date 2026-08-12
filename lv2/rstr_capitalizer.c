760bde99614292fa6193df58910b93400c7f73cf
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-12 11:23:32 +0100

    create main

--- lv2/rstr_capitalizer.c
+++ lv2/rstr_capitalizer.c
@@ -17,3 +17,20 @@ void rstr_capitalizer(char *str)
 	}
 	write(1, "\n", 1);
 }
+
+int	main(int ac, char **av)
+{
+	int	i = 1;
+
+	if (ac == 1)
+		write(1, "\n", 1);
+	else
+	{
+		while (i < ac)
+		{
+			rstr_capitalizer(ac - 1);
+			i++;
+		}
+		write(1, "\n", 1);
+	}
+}
