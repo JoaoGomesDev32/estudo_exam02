6aa91b740cb1016ce6e9bab823e6e535321ee65d
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-12 17:28:09 +0100

    adjust indent

--- lv3/fprime.c
+++ lv3/fprime.c
@@ -3,26 +3,26 @@
 
 int main(int ac, char **av)
 {
-    int div = 2;
-    
-    if (ac != 2)
-    printf("\n");
-    else
-    {
-        int n = atoi(av[1]);
-        while (n > 1)
-        {
-            if (n % div == 0)
-            {
-                printf("%d", div);
-                n /= div;
-                if (n > 1)
-                    printf("*");
-            }
-            else
-                div++;
-        }
-        printf("\n");
-    }
-    return (0);
+	int div = 2;
+
+	if (ac != 2)
+		printf("\n");
+	else
+	{
+		int n = atoi(av[1]);
+		while (n > 1)
+		{
+			if (n % div == 0)
+			{
+				printf("%d", div);
+				n /= div;
+				if (n > 1)
+					printf("*");
+			}
+			else
+				div++;
+		}
+		printf("\n");
+	}
+	return (0);
 }
