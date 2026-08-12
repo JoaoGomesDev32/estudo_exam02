9788079fb6a6a3fc85dd612e917d29d768106265
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-12 14:30:59 +0100

    improve str_capitalizer

--- lv2/rstr_capitalizer.c
+++ lv2/rstr_capitalizer.c
@@ -1,35 +1,32 @@
 #include <unistd.h>
 
-void rstr_capitalizer(char *str)
-{
-	int i = 0;
-	while (str[i])
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;  // tudo minúsculo primeiro
-		// é letra E próximo não é letra (fim de palavra)
-		if ((str[i] >= 'a' && str[i] <= 'z') &&
-			(str[i+1] < 'a' || str[i+1] > 'z') &&
-			(str[i+1] < 'A' || str[i+1] > 'Z'))
-			str[i] -= 32;  // capitaliza
-		write(1, &str[i], 1);
-		i++;
-	}
-	write(1, "\n", 1);
-}
-
 int	main(int ac, char **av)
 {
-	int	i = 1;
+	int	i;
+	int	j;
 
 	if (ac == 1)
 		write(1, "\n", 1);
-	else
+	i = 1;
+	while (i < ac)
 	{
-		while (i < ac)
+		j = 0;
+		while (av[i][j])
 		{
-			rstr_capitalizer(av[i]);
-			i++;
+			if (j == 0 || av[i][j - 1] == ' ' || av[i][j - 1] == '\t')
+			{
+				if (av[i][j] >= 'a' && av[i][j] <= 'z')
+					av[i][j] -= 32;
+			}
+			else
+			{
+				if (av[i][j] >= 'A' && av[i][j] <= 'Z')
+					av[i][j] += 32;
+			}
+			write(1, &av[i][j], 1);
+			j++;
 		}
+		write(1, "\n", 1);
+		i++;
 	}
 }
