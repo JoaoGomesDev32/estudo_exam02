2b6a93bc22bf5e9155b8c63a0b9c41b090fb9267
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-12 15:00:04 +0100

    rstr_capitalizer done

--- lv2/rstr_capitalizer.c
+++ lv2/rstr_capitalizer.c
@@ -13,15 +13,20 @@ int	main(int ac, char **av)
 		j = 0;
 		while (av[i][j])
 		{
-			if (j == 0 || av[i][j - 1] == ' ' || av[i][j - 1] == '\t')
+			if ((av[i][j] >= 'a' && av[i][j] <= 'z')
+				|| (av[i][j] >= 'A' && av[i][j] <= 'Z'))
 			{
-				if (av[i][j] >= 'a' && av[i][j] <= 'z')
-					av[i][j] -= 32;
-			}
-			else
-			{
-				if (av[i][j] >= 'A' && av[i][j] <= 'Z')
-					av[i][j] += 32;
+				if (av[i][j + 1] == '\0' || av[i][j + 1] == ' '
+					|| av[i][j + 1] == '\t')
+				{
+					if (av[i][j] >= 'a' && av[i][j] <= 'z')
+						av[i][j] -= 32;
+				}
+				else
+				{
+					if (av[i][j] >= 'A' && av[i][j] <= 'Z')
+						av[i][j] += 32;
+				}
 			}
 			write(1, &av[i][j], 1);
 			j++;
--- lv2/str_capitalizer.c
+++ lv2/str_capitalizer.c
@@ -35,3 +35,37 @@ int	main(int ac, char **av)
 			str_capitalizer(av[i++]);
 	return (0);
 }
+
+
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac == 1)
+		write(1, "\n", 1);
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (j == 0 || av[i][j - 1] == ' ' || av[i][j - 1] == '\t')
+			{
+				if (av[i][j] >= 'a' && av[i][j] <= 'z')
+					av[i][j] -= 32;
+			}
+			else
+			{
+				if (av[i][j] >= 'A' && av[i][j] <= 'Z')
+					av[i][j] += 32;
+			}
+			write(1, &av[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
