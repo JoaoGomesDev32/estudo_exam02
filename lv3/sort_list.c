07ccb672d893d90e4ceaa2e0866578a8c23d6f3a
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-13 20:20:37 +0100

    adjust indent

--- lv3/sort_list.c
+++ lv3/sort_list.c
@@ -1,27 +1,27 @@
 #include "list.h"
 
-t_list  *sort_list(t_list *lst, int (*cmp)(int, int))
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
 {
-    t_list  *cur;
-    int     temp;
-    int     swapped;
+	t_list  *cur;
+	int     temp;
+	int     swapped;
 
-    swapped = 1;
-    while (swapped)
-    {
-        swapped = 0;
-        cur = lst;
-        while (cur && cur->next)
-        {
-            if (!cmp(cur->data, cur->next->data))
-            {
-                temp = cur->data;
-                cur->data = cur->next->data;
-                cur->next->data = temp;
-                swapped = 1;
-            }
-            cur = cur->next;
-        }
-    }
-    return (lst);
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		cur = lst;
+		while (cur && cur->next)
+		{
+			if (!cmp(cur->data, cur->next->data))
+			{
+				temp = cur->data;
+				cur->data = cur->next->data;
+				cur->next->data = temp;
+				swapped = 1;
+			}
+			cur = cur->next;
+		}
+	}
+	return (lst);
 }
