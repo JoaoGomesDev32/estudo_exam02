cc105efa3320c27327ca1f530a809c8769f64f9b
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-16 17:51:50 +0100

    improve rostring

--- lv3/rostring.c
+++ lv3/rostring.c
@@ -4,45 +4,44 @@ int	main(int ac, char **av)
 {
 	int	i;
 	int	j;
-	int	word_end;
-	int	in_word;
 	int	first_word;
+	int	in_word;
+	int	end_word;
 
 	if (ac < 2)
-	{
 		write(1, "\n", 1);
-		return (0);
-	}
-	i = 0;
-	while (av[1][i] == ' ' || av[1][i] == '\t')
-		i++;
-	j = i;
-	while (av[1][j] && av[1][j] != ' ' && av[1][j] != '\t')
-		j++;
-	word_end = j;
-	in_word = 0;
-	first_word = 0;
-	while (av[1][j])
+	else
 	{
-		if (av[1][j] == ' ' || av[1][j] == '\t')
-			in_word = 0;
-		else
+		i = 0;
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		j = i;
+		while (av[1][j] && av[1][j] != ' ' && av[1][j] != '\t')
+			j++;
+		end_word = j;
+		first_word = 0;
+		in_word = 0;
+		while (av[1][j])
 		{
-			if (in_word == 0 && first_word == 1)
-				write(1, " ", 1);
-			write(1, &av[1][j], 1);
-			in_word = 1;
-			first_word = 1;
+			if (av[1][j] == ' ' || av[1][j] == '\t')
+				in_word = 0;
+			else
+			{
+				if (in_word == 0 && first_word == 1)
+					write(1, " ", 1);
+				write(1, &av[1][j], 1);
+				in_word = 1;
+				first_word = 1;
+			}
+			j++;
 		}
-		j++;
 	}
 	if (first_word)
 		write(1, " ", 1);
-	while (i < word_end)
+	while (i < end_word)
 	{
 		write(1, &av[1][i], 1);
 		i++;
 	}
 	write(1, "\n", 1);
-	return (0);
 }
